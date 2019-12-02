2a196f9eef3b33bbe13785b0200c1e1a7bc16fa1
Chen Xiongjie <chen.xcmail@gmail.com>
2019-12-02 17:39:37 +0100

    Updated: checkbox value

--- src/ui/fieldtype/Checkbox.tsx
+++ src/ui/fieldtype/Checkbox.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import Moment from 'react-moment';
 
-export default class Checkbox extends React.Component<{definition:any, beforeField:any, afterField:any, validation:any, data:any, mode?:string},{}> {
+export default class Checkbox extends React.Component<{definition:any, beforeField:any, afterField:any, validation:any, data:any, mode?:string},{checked:boolean}> {
 
 constructor(props:any) {
       super(props);
-      this.state = {};
+      this.state = {checked: this.props.data==1};
     }
 
+    changeChecked(){
+      this.setState({checked: !this.state.checked});
+    }
 
     render(){
         const BeforeElement:React.ReactType = this.props.beforeField();
@@ -16,8 +19,13 @@ constructor(props:any) {
             <div className={'field checkbox '+this.props.mode+' '+(this.props.definition.required?'required':'')+(this.props.validation=='1'?' result-required':'')}>
               {BeforeElement}
              <label>
-                {this.props.mode=='edit'&&<input type="hidden" value="-1" name={this.props.definition.identifier} />}
-                <input type="checkbox" disabled={this.props.mode!='edit'} id={this.props.definition.identifier} name={this.props.definition.identifier} defaultValue={this.props.data} />
+                {this.props.mode=='edit'&&!this.state.checked&&<input type="hidden" value="0" name={this.props.definition.identifier} />}
+                <input type="checkbox" disabled={this.props.mode!='edit'}
+                    id={this.props.definition.identifier}
+                    name={this.props.definition.identifier}
+                    value="1"
+                    onChange={this.changeChecked.bind(this)}
+                    defaultChecked={this.props.data==1} />
                 <div>{this.props.definition.name}</div>
                </label>
                 <div className="field-description">{this.props.definition.description}</div>
