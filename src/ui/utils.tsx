7a93e810b70a6846385dc220a3f39be9c61512e7
Chen Xiongjie <chen.xcmail@gmail.com>
2019-12-17 14:38:49 +0100

    Updated: set formdata array limit

--- src/ui/utils.tsx
+++ src/ui/utils.tsx
@@ -9,7 +9,7 @@ export function buildObject(form:any){
     let objectStr = arr.join('&');
 
     var qs = require('qs');
-    let dataObject = qs.parse( objectStr, {parameterLimit:1000000} );
+    let dataObject = qs.parse( objectStr, {parameterLimit:1000000, arrayLimit:100} );
     console.log( objectStr );
     return dataObject;
   };
