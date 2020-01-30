96e66df143e6e07a46c195befea3b22c473cc67a
Chen Xiongjie <chen.xcmail@gmail.com>
2020-01-30 10:24:46 +0100

    Fixed: form containing & character

--- src/ui/utils.tsx
+++ src/ui/utils.tsx
@@ -5,12 +5,16 @@ export function buildObject(form:any){
 
     let arr:Array<string> = [];
     Array.from(formData.entries()).map((item)=>{
-         arr.push( item[0]+'='+item[1] );
+        var value = item[1];
+        if( typeof( value ) == 'string' ){
+          value = encodeURIComponent(value);
+        }
+         arr.push( item[0]+'='+value );
     });
     let objectStr = arr.join('&');
 
     var qs = require('qs');
     let dataObject = qs.parse( objectStr, {parameterLimit:1000000, arrayLimit:100} );
-    console.log( objectStr );
+    console.log( dataObject );
     return dataObject;
   };
