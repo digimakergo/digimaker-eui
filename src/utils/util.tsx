c85c3c711a13a0b1c59459606b1e7b4853244efe
Chen Xiongjie <chen.xcmail@gmail.com>
2020-05-20 17:30:07 +0200

    Added: FetchwithAuth and GetAccessToken for jwt based authorization

---
+++ src/utils/util.tsx
@@ -0,0 +1,35 @@
+let refreshToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTAwODc4NjMsImd1aWQiOiI1MDExOTI4Ni1kMjkzLTQyOWMtODgxYi1hMTcxZGFiYzliNjMiLCJ1c2VyX2lkIjoxfQ.gWt78lq9CFW7s1l7B8_-eroKJ4kLm4W_shMAVIZ4RA8';
+
+
+let accessToken: any; //access token, which is a promise<string>
+export function FetchwithAuth(url:string, initObj?:any){
+  return GetAccessToken().then( token=>{
+    //create add Authorization into header
+    let authValue = 'Bearer '+token;
+    if( initObj ){
+      if(initObj.headers){
+          initObj.headers.append( 'Authorization', authValue );
+      }else{
+        initObj.headers = new Headers({'Authorization': authValue});
+      }
+    }else{
+      initObj = {headers:new Headers({'Authorization': authValue}) };
+    }
+    //fetch
+    return fetch(url, initObj)
+  })
+}
+
+//get new/renewed/cached access token, return promise(token)
+export function GetAccessToken() {
+  if (accessToken) {
+    return accessToken;
+  }
+  let fetchPro = fetch(process.env.REACT_APP_REMOTE_URL + '/auth/token/access/renew?token=' + refreshToken)
+    .then(res => {
+      accessToken = res.text();
+      return accessToken
+    });
+    //todo: add handling when fail because of invalid/network issue/other or expired
+  return fetchPro
+}
