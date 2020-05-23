1a4f467abbe6cef00bdc996f9e4ef9161164fbe0
Chen Xiongjie <chen.xcmail@gmail.com>
2020-05-23 14:10:12 +0200

    Updated: metainfo author name & style

--- src/main/MetaInfo.tsx
+++ src/main/MetaInfo.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Moment from 'react-moment';
+import { Link } from "react-router-dom";
 
 export default class MetaInfo extends React.Component<{content:any}> {
 
@@ -12,13 +13,13 @@ export default class MetaInfo extends React.Component<{content:any}> {
          <div className="block-title">Metainfo</div>
          <div className="block-body">
          <div>ID: {this.props.content.id}</div>
+         <div>CID: {this.props.content.cid}</div>
          <div>Name: {this.props.content.name}</div>
-         <div>Author: {this.props.content.author}</div>
+         <div>Author: <Link to={"/main/"+this.props.content.author}>{this.props.content.author_name}</Link></div>
          <div>Version: {this.props.content.version}</div>
          <div>Published: <Moment unix format="DD.MM HH:mm">{this.props.content.published}</Moment></div>
          <div>Modified: <Moment unix format="DD.MM HH:mm">{this.props.content.modified}</Moment></div>
          <div>Status: <span className={"status-"+this.props.content.content_type+" status-"+this.props.content.status}></span></div>
-         <div>CID: {this.props.content.cid}</div>
          <div>UID: {this.props.content.uid}</div>
          </div>
        </div>
