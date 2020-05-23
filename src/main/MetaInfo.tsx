28b6f4ac11bb738f9d2eaeebb7c103234c764c41
Chen Xiongjie <chen.xcmail@gmail.com>
2020-05-23 14:23:44 +0200

    Updated: style

--- src/main/MetaInfo.tsx
+++ src/main/MetaInfo.tsx
@@ -16,9 +16,9 @@ export default class MetaInfo extends React.Component<{content:any}> {
          <div>CID: {this.props.content.cid}</div>
          <div>Name: {this.props.content.name}</div>
          <div>Author: <Link to={"/main/"+this.props.content.author}>{this.props.content.author_name}</Link></div>
-         <div>Version: {this.props.content.version}</div>
          <div>Published: <Moment unix format="DD.MM HH:mm">{this.props.content.published}</Moment></div>
          <div>Modified: <Moment unix format="DD.MM HH:mm">{this.props.content.modified}</Moment></div>
+         <div>Version: {this.props.content.version}</div>
          <div>Status: <span className={"status-"+this.props.content.content_type+" status-"+this.props.content.status}></span></div>
          <div>UID: {this.props.content.uid}</div>
          </div>
