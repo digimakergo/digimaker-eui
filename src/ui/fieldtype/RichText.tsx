5cbec970821351b87740b2879eafdcf1f288f619
Ajith <rajith1995@gmail.com>
2020-06-11 14:48:21 +0530

    Removed TinyMce

--- src/ui/fieldtype/RichText.tsx
+++ src/ui/fieldtype/RichText.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react';
-import tinymce from "tinymce";
 import Moment from 'react-moment';
 import ReactTooltip from 'react-tooltip'
-import { Editor } from '@tinymce/tinymce-react';
+
 
 export default class RichText extends React.Component<{ definition: any, validation: any, beforeField:any, afterField: any, data: any, mode: string }, {}> {
 
   constructor(props: any) {
     super(props);
     this.state = {};
-  }
-
-  handleEditorChange = (content, editor) => {
-    console.log('Content was updated:', content);
-    this.setState({data:content})
   }
 
   edit() {
@@ -27,25 +21,7 @@ export default class RichText extends React.Component<{ definition: any, validat
           {this.props.definition.description && <i className="icon-info" data-for={this.props.definition.identifier+'-desciption'} data-tip=""></i>}
           {this.props.definition.description&&<ReactTooltip id={this.props.definition.identifier+'-desciption'} effect="solid" place="right" html={true} clickable={true} multiline={true} delayHide={500} className="tip">{this.props.definition.description}</ReactTooltip>}
           : </label>
-
-          <Editor
-              value={this.props.data}
-              init={{
-                menubar: false,
-                branding: false,
-                plugins: [
-                  'advlist autolink lists link image charmap print preview anchor',
-                  'searchreplace visualblocks code fullscreen',
-                  'insertdatetime media table paste code'
-                ],
-                toolbar:
-                  'undo redo | formatselect | bold italic backcolor | \
-                  alignleft aligncenter alignright alignjustify | \
-                  bullist numlist outdent indent | removeformat | help'
-              }}
-              onEditorChange={this.handleEditorChange}> 
-                <textarea id={this.props.definition.identifier} className="form-control" name={this.props.definition.identifier} defaultValue={this.props.data}></textarea>
-          </Editor>
+        <textarea id={this.props.definition.identifier} className="form-control" name={this.props.definition.identifier} defaultValue={this.props.data}></textarea>
         {AfterElement}
       </div>
     )
@@ -54,14 +30,12 @@ export default class RichText extends React.Component<{ definition: any, validat
   view(){
     const BeforeElement:React.ReactType = this.props.beforeField();
     const AfterElement:React.ReactType = this.props.afterField();
-    return (
-            <div className={'view field ' + this.props.definition.type}>
+    return (<div className={'view field ' + this.props.definition.type }>
             {BeforeElement}
               <label>{this.props.definition.name}: </label>
               <div className="field-value">{this.props.data}</div>
             {AfterElement}
-            </div>
-            )
+            </div>)
   }
 
   render() {
