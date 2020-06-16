358781887f40bd631895dd0869beb1c1d49ebb75
Ajith <rajith1995@gmail.com>
2020-06-16 14:13:52 +0530

    Edited: TinyMCE Cloud Loading

--- src/ui/fieldtype/RichText.tsx
+++ src/ui/fieldtype/RichText.tsx
@@ -2,34 +2,16 @@ import * as React from 'react';
 import tinymce from "tinymce";
 import Moment from 'react-moment';
 import ReactTooltip from 'react-tooltip'
-
-var defaultToolbar=  'undo redo | formatselect | bold italic textcolor | \
-  alignleft aligncenter alignright alignjustify | \
-  bullist numlist outdent indent  | image charmap media | insertdatetime table paste | code preview print importcss | removeformat';
-
-  var defaultPlugin = [
-    'advlist autolink lists charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen textcolor',
-    'insertdatetime media table paste code importcss'
-  ];
+import "tinymce/themes/modern";
 
 export default class RichText extends React.Component<{ definition: any, validation: any, beforeField:any, afterField: any, data: any, mode: string }, {data:''}> {
 
-    
   constructor(props: any) {
     super(props);
     this.state = {data:''};
   }
 
-  
-
   componentDidMount() {
-    
-    this.setState({data:this.props.data});
-
-    if(this.props.definition == null){
-      defaultToolbar = ''
-    }
     
     tinymce.init({
       menubar:false,
@@ -45,6 +27,7 @@ export default class RichText extends React.Component<{ definition: any, validat
         });
       },
     });
+    this.setState({data:this.props.data});
   }
 
   edit() {
@@ -58,9 +41,7 @@ export default class RichText extends React.Component<{ definition: any, validat
           {this.props.definition.description && <i className="icon-info" data-for={this.props.definition.identifier+'-desciption'} data-tip=""></i>}
           {this.props.definition.description&&<ReactTooltip id={this.props.definition.identifier+'-desciption'} effect="solid" place="right" html={true} clickable={true} multiline={true} delayHide={500} className="tip">{this.props.definition.description}</ReactTooltip>}
           : </label>
-              
-            <textarea id={this.props.definition.identifier} className="form-control" name={this.props.definition.identifier} value={this.state.data} defaultValue={this.props.data}></textarea>
-
+          <textarea id={this.props.definition.identifier} className="form-control" name={this.props.definition.identifier} value={this.state.data} defaultValue={this.props.data}></textarea>
         {AfterElement}
       </div>
     )
