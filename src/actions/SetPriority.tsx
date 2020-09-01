6999f2b4c3a18b6c11607c676e5f3a9e817e6936
Chen Xiongjie <chen.xcmail@gmail.com>
2020-09-01 14:03:33 +0200

    Added: set priority(move to top/remove priority) in row action

---
+++ src/actions/SetPriority.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {FetchWithAuth} from 'digimaker-ui/util';
+
+export default class SetPriority extends React.Component<{from:any, afterAction:any}, {}> {
+  private priortyStep = 100;
+
+  constructor(props: any) {
+      super(props);
+  }
+
+
+  removePriority(){
+    let content = this.props.from;
+    this.setPriority(content.id, 0);
+  }
+
+  setPriority(id:number, priority:number){
+    FetchWithAuth(process.env.REACT_APP_REMOTE_URL + '/content/setpriority?params='+id+','+priority)
+      .then((res:any)=>res.json())
+      .then((data:any)=>{
+          this.props.afterAction();
+      });
+  }
+
+  setToTop(){
+    let content = this.props.from;
+    FetchWithAuth(process.env.REACT_APP_REMOTE_URL + '/content/list/'+content.content_type+'?parent='+content.parent_id+'&sortby=priority%20desc&limit=1&offset=0')
+      .then((res:any)=>res.json())
+      .then((data:any)=>{
+        let priority = this.priortyStep;
+        if( data.list.length > 0 ){
+            let topPriority = data.list[0].priority;
+            priority = topPriority+this.priortyStep;
+        }
+        this.setPriority( content.id, priority );
+      });
+  }
+
+  render(){
+    let priority = this.props.from.priority;
+    return (<div>{priority!=0&&<span onClick={()=>this.removePriority()}>Remove priority</span>}
+                 {priority==0&&<span onClick={()=>this.setToTop()}>Set top priority</span>}</div>)
+  }
+}
