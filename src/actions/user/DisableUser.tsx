e507153f60ba9a2e547b61c57a9f34385e7a8b72
Chen Xiongjie <chen.xcmail@gmail.com>
2021-02-18 15:58:55 +0100

    Added: action enable/disable user

---
+++ src/actions/user/DisableUser.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {FetchWithAuth} from 'digimaker-ui/util';
+import util from 'digimaker-ui/util';
+import Browse from 'digimaker-ui/Browse';
+import {Dialog} from 'digimaker-ui/util';
+
+
+
+export default class DisableUser extends React.Component<{from:any, selected?:any, afterAction?:any}, {counter: number, message: string}> {
+  constructor(props: any) {
+      super(props);
+      this.state ={counter: 0, message:''};
+  }
+
+  execute(enable: boolean){
+    this.setState({counter: this.state.counter+1});
+    let selected = this.props.selected;
+    if( selected.length == 0 ){
+      this.setState({message: 'No item selected in the list.'});
+    }else{
+      let ids:any = [];
+      selected.map((item:any)=>{
+        ids.push( item.cid );
+      });
+      FetchWithAuth( process.env.REACT_APP_REMOTE_URL + '/user/enable/'+(enable?1:0)+'?id='+ids.join( ',' ) )
+        .then(res=>res.text()).then((data)=>{
+          this.props.afterAction( true );
+        }).catch(()=>{
+          this.setState({message: 'Error'});
+        });
+    }
+  }
+
+  render(){
+    return <div>
+        {this.state.message&& <Dialog type="confirm" onClose={()=>this.setState({message: ''})} title="Disable/enable user">{this.state.message}</Dialog>}
+        <button className="btn btn-sm btn-link" onClick={()=>this.execute(false)}>Disable</button>&nbsp;
+        <button className="btn btn-sm btn-link" onClick={()=>this.execute(true)}>Enable</button>
+        </div>;
+  }
+}
