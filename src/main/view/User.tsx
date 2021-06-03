bb1dc4c8e6f8ced9fbcb4d475d5c1e9ba4b8b953
Chen Xiongjie <chen.xcmail@gmail.com>
2021-06-03 14:55:59 +0200

    added: user role in user view

---
+++ src/main/view/User.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import {FetchWithAuth} from 'digimaker-ui/util';
+import List from 'digimaker-ui/List';
+import {Link} from 'react-router-dom';
+import Browse from 'digimaker-ui/Browse';
+import util from 'digimaker-ui/util';
+import config from '../../dm.json';
+
+export default class User extends React.Component<{content:any},{browse:boolean, data:any}> {
+  constructor(props: any) {
+    super(props);
+    this.state = { browse:false, data: '' };
+  }
+
+  componentDidMount(){
+    this.fetchRoles()
+  }
+
+  fetchRoles(){
+    FetchWithAuth(process.env.REACT_APP_REMOTE_URL + '/user/roles/'+this.props.content.cid)
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({data:data});
+        } );
+  }
+
+  addRole(e:any){
+    this.setState({browse: true});
+  }
+
+  confirmAdding(selected){
+    let ids:any = [];
+    for( let item of selected ){
+      let existing = this.state.data.find( (i)=>i.id == item.id );
+      if( !existing ){
+        ids.push(item.id);
+      }
+    }
+
+    FetchWithAuth(process.env.REACT_APP_REMOTE_URL + '/access/assign/'+ids.join(',')+'/'+this.props.content.cid)
+        .then(res => res.json())
+        .then((data) => {
+          this.fetchRoles();
+        } );
+    this.setState({browse:false});
+  }
+
+  unassignRole(e:any, roleId:any){
+    e.preventDefault();
+    FetchWithAuth(process.env.REACT_APP_REMOTE_URL + '/access/unassign/'+this.props.content.cid+'/'+roleId)
+        .then(res => res.json())
+        .then((data) => {
+          this.fetchRoles();
+        } );
+  }
+
+  render(){
+    let config = {};
+    if( !this.state.data ){
+      return '';
+    }
+    return <div className="block">
+          <h3>
+            Roles &nbsp;
+            <a href="#" onClick={(e)=>this.addRole(e)}><i className="fas fa-plus"></i> Add</a>
+            {this.state.browse&&<Browse config={util.getConfig().browse} multi={true} selected={this.state.data} onConfirm={(selected)=>this.confirmAdding(selected)} onClose={()=>this.setState({browse:false})} trigger={true} contenttype={["role"]} />}
+          </h3>
+          <ul>
+          {this.state.data.map( (item)=><li>
+              <Link to={"/main/"+item.id}>{item.name}</Link> <a href="#" onClick={(e)=>this.unassignRole( e, item.cid )}><i className="icon icon-delete"></i></a>
+            </li> )}
+          </ul>
+      </div>;
+  }
+}
